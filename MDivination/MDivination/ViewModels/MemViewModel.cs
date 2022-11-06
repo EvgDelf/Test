f6e4eba39e514bdcd2256986c671c1842037cdae
Evgeniy Delfontsev <devgen9275@gmail.com>
2022-11-06 23:04:40 +0300

    First commit
    Add App

---
+++ MDivination/MDivination/ViewModels/MemViewModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Windows.Input;
+using MDivination.Models;
+using Xamarin.Forms;
+using MDivination.Views;
+using System.Runtime.InteropServices.ComTypes;
+using System.Collections.ObjectModel;
+using System.Reflection;
+
+
+namespace MDivination.ViewModels
+{
+    public class MemViewModel : INotifyPropertyChanged
+    {
+       
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string name)
+                {
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+                }
+
+        public ICommand DivinationCommand { get; set; }
+        public ICommand BackCommand { get; set; }
+        public ICommand HistoryCommand { get; set; }
+        public INavigation Navigation;
+        public Mem Mem;
+        public static Random random=new Random();
+        public static int i
+        {
+            get => i;
+            set
+            {
+                i = value;
+            }
+        }
+        public static string ImagePath = $"MDivination.Images.{i}.png";
+      
+        public ImageSource ImageSource => ImageSource.FromResource(ImagePath);
+
+        public string interpretation;
+
+        public string Interpretation
+        {
+            get => interpretation;
+            set
+            {
+                Stream streamInterpretation = Assembly.GetExecutingAssembly().GetManifestResourceStream($"MDivination.Interpretation.{i}.txt");
+                TextReader readerInterpretation = new StreamReader(streamInterpretation);
+                interpretation = readerInterpretation.ReadToEnd();
+                streamInterpretation.Close();
+                OnPropertyChanged(nameof(Interpretation));
+            }
+        }
+        public string history;
+        public string History
+        {
+            get => history;
+            set
+            {
+                Stream streamHistory = Assembly.GetExecutingAssembly().GetManifestResourceStream($"MDivination.History.{i}.txt");
+                TextReader readerHistory = new StreamReader(streamHistory);
+                interpretation = readerHistory.ReadToEnd();
+                streamHistory.Close();
+                OnPropertyChanged(nameof(History));
+            }
+        }
+        public MemViewModel()
+        {
+            Mem = new Mem();
+            DivinationCommand = new Command(Divination);
+            BackCommand = new Command(Back);
+            HistoryCommand = new Command(ToHistory);
+            
+        }
+      
+       
+
+        void Divination(object sender)
+        {
+                Navigation.PushAsync(new DivinationResult());
+        }
+        void Back(object sender)
+        {
+            Navigation.PopToRootAsync();
+        }
+        void ToHistory(object sender)
+        {
+            Navigation.PushAsync(new HistoryPage());
+        }
+
+    }
+}
---
+++ MDivination/MDivination/Views/DivinationResult.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using MDivination.ViewModels;
+
+
+namespace MDivination.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DivinationResult : ContentPage
+    {
+       
+        
+        public DivinationResult()
+        {
+            InitializeComponent();
+
+            BindingContext = new MemViewModel();
+        }
+
+           
+
+
+       
+
+    }
+    }
